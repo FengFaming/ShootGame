884957f00f88539b018b21d0be38845b2d6d008c
紫雪 <ffmlly9112160805@163.com>
2020-01-09 18:27:07 +0800

    项目文件相关提交
    剔除关联文件

---
+++ Assets/Engine/Editor/MathTest.cs
@@ -0,0 +1,23 @@
+/*
+ * Creator:ffm
+ * Desc:一些数据的测试
+ * Time:2019/12/26 16:26:48
+* */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Engine;
+using UnityEditor;
+
+public class MathTest : ObjectBase
+{
+	[MenuItem("Tools/MathTest")]
+	private static void MathT()
+	{
+		//int[] t = new int[1];
+		//t[0] = 1;
+		//Array.Copy()
+	}
+}
